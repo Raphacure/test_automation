49968e83fe5346f0b9889aec5e4a4eedf83a36e9
pavankumarsadasiva <pavans5699@gmail.com>
2025-06-27 18:06:39 +0530

    search functonality

--- cypress/e2e/Doctor_consultation/DoctorConsultationSearch.cy.js
+++ cypress/e2e/Doctor_consultation/DoctorConsultationSearch.cy.js
@@ -41,69 +41,67 @@ describe('Doctor Consultation Search and Booking', () => {
             cy.contains('.search-popup-name', 'Naveen Gowda', { timeout: 12000 })
                 .should('be.visible')
                 .click();
-         cy.url().should('include', 'https://raphacure.com/doctor/doctordetails/22463');
+             cy.url().should('include', 'https://raphacure.com/doctor/doctordetails/22463');
 
-   
-         cy.wait(2000); // allow time for page to load
+            cy.wait(2000); // allow time for page to load
 
-    // Step 4: Choose Booking Options
-    cy.get('#virtual-types-list1').check().should('be.checked');
-    
-    cy.get('.day-button').eq(1).click();
-
-    // Step 5: Choose Random Time Slot
-    cy.get('.time-slots-div .slot-button')
-      .should('have.length.greaterThan', 0)
-      .then($buttons => {
-        const count = $buttons.length;
-        const randomIndex = Math.floor(Math.random() * count);
-        cy.wrap($buttons[randomIndex]).click();
-      });
-
-    // Step 6: Proceed to Checkout
-    cy.get('.pay-proceed').should('be.visible').click();
-    
-    cy.url().should('include', '/checkout');
-    // Optional: PhonePe payment test (mock or conditionally check)
-      
-
-      //  cy.get('.checkout-pay-buttom.phonePe button').click();
-      cy.get('.checkout-pay-buttom.checkout-btn-blue button').first().click();
-
-      cy.get('iframe[class*="razorpay-checkout-frame"]', { timeout: 10000 })
-        .should('be.visible')
-        .then($iframe => {
-         const body = $iframe[0].contentDocument.body;
-
-        });
-     
-
-        // cy.get('button[data-testid="checkout-close"]').should('be.visible').click();
-       cy.get('iframe')
-        .its('0.contentDocument.body').should('not.be.empty')
-         .then(cy.wrap).find('button[data-testid="checkout-close"]').should('be.visible').click();
-
-       cy.get('iframe')
-        .its('0.contentDocument.body').should('not.be.empty').then(cy.wrap)
-        .find('button[data-testid="confirm-positive"]').should('be.visible').click()
-        .then(() => cy.log('Iframe close clicked'));
-        cy.wait(1000); 
-       
-      // data-testid="confirm-positive"
-      cy.contains('Payment failed. Please retry from patient dashboard', { timeout: 3000 })
-        .should('be.visible')
-        .then(() => {
-          cy.log('Modal is visible');
+            // Step 4: Choose Booking Options
+          cy.get('#virtual-types-list1').check().should('be.checked');
           
-          cy.get('.modal-content').should('be.visible').within(() => {
-            cy.contains('button', 'Ok')
-              .should('be.visible')
-              .click();
-          });
-        });
+          cy.get('.day-button').eq(1).click();
 
-     
-             cy.url().should('include', 'https://raphacure.com/dashboard/bookings');
+          // Step 5: Choose Random Time Slot
+          cy.get('.time-slots-div .slot-button')
+            .should('have.length.greaterThan', 0)
+            .then($buttons => {
+              const count = $buttons.length;
+              const randomIndex = Math.floor(Math.random() * count);
+              cy.wrap($buttons[randomIndex]).click();
+            });
+
+          // Step 6: Proceed to Checkout
+          cy.get('.pay-proceed').should('be.visible').click();
+          
+          cy.url().should('include', '/checkout');
+          // Optional: PhonePe payment test (mock or conditionally check)
+        
+
+          //  cy.get('.checkout-pay-buttom.phonePe button').click();
+            cy.get('.checkout-pay-buttom.checkout-btn-blue button').first().click();
+
+            cy.get('iframe[class*="razorpay-checkout-frame"]', { timeout: 10000 })
+              .should('be.visible')
+              .then($iframe => {
+              const body = $iframe[0].contentDocument.body;
+
+              });
+          
+
+            // cy.get('button[data-testid="checkout-close"]').should('be.visible').click();
+          cy.get('iframe')
+            .its('0.contentDocument.body').should('not.be.empty')
+            .then(cy.wrap).find('button[data-testid="checkout-close"]').should('be.visible').click();
+
+          cy.get('iframe')
+            .its('0.contentDocument.body').should('not.be.empty').then(cy.wrap)
+            .find('button[data-testid="confirm-positive"]').should('be.visible').click()
+            .then(() => cy.log('Iframe close clicked'));
+            cy.wait(1000); 
+          
+          // data-testid="confirm-positive"
+          cy.contains('Payment failed. Please retry from patient dashboard', { timeout: 3000 })
+            .should('be.visible')
+            .then(() => {
+              cy.log('Modal is visible');
+              
+              cy.get('.modal-content').should('be.visible').within(() => {
+                cy.contains('button', 'Ok')
+                  .should('be.visible')
+                  .click();
+              });
+            });
+
+          cy.url().should('include', 'https://raphacure.com/dashboard/bookings');
         });
        
 });
